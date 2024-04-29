cf521aba5e1f8b6a8a970e35ecf6b0f8afe6f9c2
Bhushan-Dudhal <bhushandhudhal9@gmail.com>
2024-04-29 23:55:37 +0530

    simple js

---
+++ script.js
@@ -0,0 +1,7 @@
+ function change_col(color) {
+        document.getElementById("abcd").style.backgroundColor = color;
+
+        if (color == "black") {
+          document.getElementById("abcd").style.backgroundColor = "white";
+        }
+      }
